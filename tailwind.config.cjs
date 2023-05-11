b3bc674fe71c66e82861582246bded0bbd97be72
roadToCoder <djaffre.prog@gmail.com>
2023-05-11 12:59:04 +0200

    Modification

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,37 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-    extend: {
-    },
+		extend: {
+			keyframes: {
+			  flipVertical: {
+				'50%': { transform: 'rotateX(180deg)' },
+			  },
+			  itineraire: {
+				'0%': {
+					opacity: '0',
+				  	transform: 'translateY(-12px)',
+				},
+				'100%': {
+					opacity: '1',
+				 	transform: 'translateY(0px)',
+				},
+			  },
+			  reconversion: {
+				'0%': {
+					opacity: '0',
+				  	transform: 'translateY(-12px)',
+				},
+				'100%': {
+					opacity: '1',
+				 	transform: 'translateY(0px)',
+				},
+			  },
+			},
+			animation: {		  
+			  itineraire: 'itineraire 0.7s',
+			  reconversion: 'reconversion 0.9s',
+			},
+		  },
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
